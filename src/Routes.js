0bb02369d23d1dbbcd325713d2b44dde5e0f0f74
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-05-14 17:26:27 -0600

    Feat(): Removing some unused files and also modifyng some rules on eslint, added also routes file in order to load there the routes

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+
+
+const GameSpotRoutes = () => (
+  <Router>
+    <Routes>
+      {/* <Route path='/login' element={<LoginComponent />} /> */}
+    </Routes>
+  </Router>
+)
+
+
+
+export default GameSpotRoutes
